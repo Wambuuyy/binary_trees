a2606dd31a2478dcdeb23c72446d9fb97981b2ac
Prudence Wambui <wambuiprudy@gmail.com>
2024-02-29 09:36:55 +0300

    add description

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,6 +3,7 @@
 /**
  * binary_tree_size - that measures the size of a binary tree
  * @tree: pointer node binary tree
+ * Description: this is the total number of nodes present
  * Return: measures the size of a binary tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
